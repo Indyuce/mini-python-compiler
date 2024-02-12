2091d60ab431b14494c3d26d2f08e88f5013d5d7
Jules <peyrat.jules@yahoo.fr>
2024-02-12 17:25:17 +0100

    skeleton for code generation

--- src/mini_python/Compile.java
+++ src/mini_python/Compile.java
@@ -2,10 +2,122 @@ package mini_python;
 
 class Compile {
 
-  static boolean debug = false;
+    static boolean debug = false;
 
-  static X86_64 file(TFile f) {
-    return null; // TODO
-  }
+    static X86_64 file(TFile f) {
+        final X86_64 file = new X86_64();
 
+
+
+
+        return file; // TODO
+    }
+
+    class VisitorImpl implements TVisitor {
+
+        @Override
+        public void visit(Cnone c) {
+
+        }
+
+        @Override
+        public void visit(Cbool c) {
+
+        }
+
+        @Override
+        public void visit(Cstring c) {
+
+        }
+
+        @Override
+        public void visit(Cint c) {
+
+        }
+
+        @Override
+        public void visit(TEcst e) {
+
+        }
+
+        @Override
+        public void visit(TEbinop e) {
+
+        }
+
+        @Override
+        public void visit(TEunop e) {
+
+        }
+
+        @Override
+        public void visit(TEident e) {
+
+        }
+
+        @Override
+        public void visit(TEcall e) {
+
+        }
+
+        @Override
+        public void visit(TEget e) {
+
+        }
+
+        @Override
+        public void visit(TElist e) {
+
+        }
+
+        @Override
+        public void visit(TErange e) {
+
+        }
+
+        @Override
+        public void visit(TElen e) {
+
+        }
+
+        @Override
+        public void visit(TSif s) {
+
+        }
+
+        @Override
+        public void visit(TSreturn s) {
+
+        }
+
+        @Override
+        public void visit(TSassign s) {
+
+        }
+
+        @Override
+        public void visit(TSprint s) {
+
+        }
+
+        @Override
+        public void visit(TSblock s) {
+
+        }
+
+        @Override
+        public void visit(TSfor s) {
+
+        }
+
+        @Override
+        public void visit(TSeval s) {
+
+        }
+
+        @Override
+        public void visit(TSset s) {
+
+        }
+    }
 }
