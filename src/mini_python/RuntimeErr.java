f39439a3f4a388220c3b23edbf6cd28693c53ae2
Jules <peyrat.jules@yahoo.fr>
2024-02-26 17:54:25 +0100

    runtime errors commit 2

--- src/mini_python/RuntimeErr.java
+++ src/mini_python/RuntimeErr.java
@@ -16,6 +16,14 @@ public class RuntimeErr {
         compileThrow(v);
     }
 
+    public static void invalidIndexType(TVisitor v, Type callerType, String callerFunction) {
+        v.x86().movq("$" + ErrorEnum.INVALID_INDEX_TYPE.lbl, "%rdi");
+        v.x86().movq("$" + Type.methodNameLabel(callerFunction), "%rsi");
+        v.x86().movq(callerType.classDesc(), "%rdx");
+        v.x86().movq("8(%rdx)", "%rdx");
+        compileThrow(v);
+    }
+
     private static void compileThrow(TVisitor v) {
         v.x86().call("__print__"); // print error msg
         v.x86().call("__er__"); // exit program
@@ -23,6 +31,7 @@ public class RuntimeErr {
 
     enum ErrorEnum {
         METHOD_NOT_DEFINED("method '%s' not defined for type '%s'"),
+        INVALID_INDEX_TYPE("requires index of type int, given '%s'"),
         INVALID_ARG_TYPE("method '%s' of type '%s' does not accept arg of type '%s'"),
 
         ;
